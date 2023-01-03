6288303624d6b17fd9275dff8a996b51363d06e1
AJin <2375769460@qq.com>
2023-01-03 18:24:09 +0800

    first commit

---
+++ src/main/java/com/ajin/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ajin.git;
+
+/**
+ * @author ajin
+ * @create 2023-01-03 18:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello ajin");
+    }
+}
