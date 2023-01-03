ac22759427a3784f408798e6f3e2219af83a80ca
AJin <2375769460@qq.com>
2023-01-03 19:42:39 +0800

    hot-fix commit2

--- src/main/java/com/ajin/git/GitTest.java
+++ src/main/java/com/ajin/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello ajin2");
         System.out.println("hello ajin3");
         System.out.println("hello ajin4");
+        System.out.println("hot-fix test!");
     }
 }
