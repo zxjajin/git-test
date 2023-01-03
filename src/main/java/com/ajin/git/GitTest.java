66820281462f1a3b3c044c4d82974866ff9d61d3
AJin <2375769460@qq.com>
2023-01-03 18:30:26 +0800

    third commit

--- src/main/java/com/ajin/git/GitTest.java
+++ src/main/java/com/ajin/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello ajin");
         System.out.println("hello ajin2");
+        System.out.println("hello ajin3");
     }
 }
